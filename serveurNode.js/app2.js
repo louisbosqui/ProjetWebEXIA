1a26f565cca683b586801b1bc860a47c6c5dfd70
Ammar Diarra <ammar.diarra@viacesi.fr>
2019-01-22 15:12:32 +0100

    j'espere que queelq'un lira ce commit un jour

--- serveurNode.js/app2.js
+++ serveurNode.js/app2.js
@@ -7,30 +7,33 @@ var con = mysql.createConnection({
   host: "localhost",
   user: "qoodboop",
   password: "qoodboop",
-   database: "test"
+   database: "test1"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-   //var sql = "SELECT * FROM `users` ";
-  //con.query(sql, function (err, result) {
-    //if (err) throw err;
-    //console.log(result);
- // });
+   var sql = "SELECT * FROM `TB_USER` ";
+   con.query(sql, function (err, result) {
+   if (err) throw err;
+    console.log(result);
+  });
 });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+const hostname = '127.0.0.1';
+const port = '3001';
+
+
+console.log(`Server running at http://${hostname}:${port}/`);
+
 
 app.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+ app.get('/', function (req, res) {
+   res.send('acceuil')
+ })
 
 app.post('/', function (req, res) {
   res.send('POST request to the homepage')
@@ -52,3 +55,24 @@ app.get('/list', function (req, res) {
     res.send(result)
   });
    });
+
+
+//TEST GET AVEC PARAMS 
+
+app.get('/api/users', function(req, res) {
+  var user_id = req.param('id');
+  var token = req.param('token');
+  var geo = req.param('geo');  
+
+  res.send(user_id + ' ' + token + ' ' + geo);
+});
+
+// POST http://localhost:8080/api/users
+// parameters sent with 
+app.post('/api/users', function(req, res) {
+    var user_id = req.body.id;
+    var token = req.body.token;
+    var geo = req.body.geo;
+
+    res.send(user_id + ' ' + token + ' ' + geo);
+});

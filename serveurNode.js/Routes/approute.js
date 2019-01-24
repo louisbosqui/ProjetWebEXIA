492c9f4dcd76f65d0dcaf7c7831f51a453b46b7a
Ammar Diarra <ammar.diarra@viacesi.fr>
2019-01-24 14:45:41 +0100

    test

--- serveurNode.js/Routes/approute.js
+++ serveurNode.js/Routes/approute.js
@@ -1,4 +1,42 @@
 'use strict';
+
+// api-routes.js
+// Initialize express router
+let router = require('express').Router();
+// Set default API response
+router.get('/', function (req, res) {
+    res.json({
+        status: 'API Its Working',
+        message: 'Welcome to RESTHub crafted with love!',
+    });
+});
+// Import contact controller
+var contactController = require('./contactController');
+// Contact routes
+router.route('/contacts')
+    .get(contactController.index)
+    .post(contactController.new);
+router.route('/contacts/:contact_id')
+    .get(contactController.view)
+    .patch(contactController.update)
+    .put(contactController.update)
+    .delete(contactController.delete);
+// Export API routes
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = function(app) {
   var todoList = require('../controllers/todoListController');
 
--- serveurNode.js/model/methodes.js
+++ serveurNode.js/model/methodes.js
@@ -27,13 +27,91 @@ let USER = {
     return 
   },
   GetUserAll:function(){
-
+    var sql = "SELECT * FROM `TB_USER` ";
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+    //console.log(result);
+    res.send(result)
+    }
   },
   PostUser:function(){
+    let nom = req.body.Nom;
+    let  user = req.body.Prenom;
+    let mail = req.body.Mail;
+    let mdp = req.body.Mdp;
+    let co = req.body.Etat;
 
+      let sql = "INSERT INTO `TB_USER` (`ID_Utilisateur`, `Nom_Utilisateur`, `Prenom_Utilisateur`, `Mail_Utilisateur`, `Mdp_Utilisateur`, `Etat_Connexion_Utilisateur`) VALUES (NULL,'"+nom+"','"+user+"','"+mail+"','"+mdp+"','"+co+"')";
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.send(result); 
+    });
   },
   PutUser:function(){
+  
+  let id = req.body.ID ;
+  let nom = req.body.Nom;
+  let  user = req.body.Prenom;
+  let mail = req.body.Mail;
+  let mdp = req.body.Mdp;
+  let co = req.body.Etat;
+  let sql = "UPDATE `TB_USER` SET  Nom_Utilisateur = '"+nom+"' WHERE ID_Utilisateur="+id+"";
 
+  if(id) {
+
+        if(nom) {
+ 
+              console.log(sql);
+                con.query(sql, function (err, response) {
+                  if (err) throw err;
+                  console.log('TEST1');
+                  //console.log(response);
+                  //res.end(response); 
+                });  
+        } 
+             if(user){
+
+               sql = "UPDATE `TB_USER` SET  Prenom_Utilisateur = '"+user+"' WHERE ID_Utilisateur="+id+"";
+              console.log(sql);
+                con.query(sql, function (err, response) {
+                  if (err) throw err;
+                                    console.log('TEST2');
+
+                  //console.log(response);
+                  //res.end(response); 
+                });
+             }
+                if (mail){
+
+              sql = "UPDATE `TB_USER` SET  Mail_Utilisateur = '"+mail+"' WHERE ID_Utilisateur="+id+"";
+              console.log(sql);
+                con.query(sql, function (err, result) {
+                  if (err) throw err;
+                  //console.log(result);
+                  //res.end(result); 
+                });
+             }
+             if (mdp){
+
+                sql = "UPDATE `TB_USER` SET  Mdp_Utilisateur = '"+mdp+"' WHERE ID_Utilisateur="+id+"";
+                console.log(sql);
+                con.query(sql, function (err, result) {
+                  if (err) throw err;
+                  //console.log(result);
+                  //res.end(result); 
+                });
+             }
+                if (co){
+                 sql = "UPDATE `TB_USER` SET  Etat_Connexion_Utilisateur = '"+co+"' WHERE ID_Utilisateur="+id+"";
+                console.log(sql);
+                  con.query(sql, function (err, result) {
+                    if (err) throw err;
+                    //console.log(result);
+                    //res.end(result); 
+                  });
+               };
+    };
   }
 
 }

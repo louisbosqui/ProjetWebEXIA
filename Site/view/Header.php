335fb4f6b4ce7c6c0d8ded4f659bac27a88edda3
Ammar Diarra <ammar.diarra@viacesi.fr>
2019-01-28 11:32:12 +0100

    renomage en .php dess fichiers de la vue contenant l'extension .html

--- Site/view/Header.php
+++ Site/view/Header.php
@@ -1,7 +1,17 @@
 
+<?php 
+$client = new GuzzleHttp\Client();
+$res = $client->request('GET', 'https://api.github.com/user', [
+    'auth' => ['user', 'pass']
+]);
+echo $res->getStatusCode();
+// "200"
+echo $res->getHeader('content-type');
+// 'application/json; charset=utf8'
+echo $res->getBody();
+// {"type":"User"...'
 
-
-
+?>
 
 
 
